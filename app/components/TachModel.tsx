74743ffa9680bfb4eb3aae73702bcda659194f70
joe joe <youssefsherif469@gmail.com>
2024-08-08 17:09:36 +0300

    techModel fix

--- app/components/TachModel.tsx
+++ app/components/TachModel.tsx
@@ -1,11 +1,12 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { useSpring, animated } from "@react-spring/three";
 import { Float, useTexture } from "@react-three/drei";
 import { Decal } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
 import {  useBox } from "@react-three/cannon";
-import { useDrag } from "@use-gesture/react";
+import { DragState, useDrag } from "@use-gesture/react";
+import * as THREE from 'three';
+import { ThreeEvent } from "@react-three/fiber";
 
 
 interface TeachModelInterface {
@@ -20,7 +21,7 @@ const TechModel = ({ img, shape, pos }: TeachModelInterface) => {
 
   
 
-  const [meshRef, api] = useBox(() => ({
+  const [meshRef , api] = useBox<THREE.Mesh>(() => ({
     position: pos,
     mass: 1,
     onCollide: () =>{
@@ -28,23 +29,22 @@ const TechModel = ({ img, shape, pos }: TeachModelInterface) => {
       }
   }));
 
+
   // Define the constraints for X and Y axes
   const xMin = -5;
   const xMax = 5;
   const yMin = -5;
   const yMax = 5;
-  const zMin = 0;
-  const zMax = 5;
 
   // Use useDrag hook to handle dragging with constraints
-  const bind = useDrag((state) => {
-    const { offset: [x, y] } = state;
+  const bind : any = useDrag((state: DragState) => {
+    const { offset: [x, y] , memo = pos } = state;
     const newPosX = Math.max(xMin, Math.min(xMax, pos[0] + x * 0.01));
     const newPosY = Math.max(yMin, Math.min(yMax, pos[1] - y * 0.01));
     // Update position directly with drag delta while applying constraints
     api.position.set(newPosX, newPosY, pos[2]);
 
-  
+    return memo;
 
   });
 
@@ -58,9 +58,6 @@ const TechModel = ({ img, shape, pos }: TeachModelInterface) => {
 
 
 
-  
-
-
   return (
     <Float
       position={[1, 1, -1]}
@@ -69,16 +66,16 @@ const TechModel = ({ img, shape, pos }: TeachModelInterface) => {
       floatIntensity={2}
       speed={0.75}
     >
-    <animated.mesh
-      ref={meshRef}
-      {...bind()}
-      position={pos}
-      onPointerOver={() => setHovered(true)}
-      onPointerOut={() => setHovered(false)}
-      scale={scale}
-      castShadow
-      receiveShadow
-    >
+      <animated.mesh
+        ref={meshRef}
+        {...bind()}
+        position={pos}
+        onPointerOver={(event:  ThreeEvent<PointerEvent>) => setHovered(true)} // Explicit type for ThreeEvent
+        onPointerOut={(event:  ThreeEvent<PointerEvent>) => setHovered(false)} // Explicit type for ThreeEvent
+        scale={scale}
+        castShadow
+        receiveShadow
+      >
       <animated.meshStandardMaterial color={color} polygonOffset polygonOffsetFactor={-5} flatShading />
       <Decal map={decal} position={[0, 0, 0.5]} rotation={[2*Math.PI , 0 , 6.25]} />
       {shape}
